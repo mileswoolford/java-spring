0d1c56ca2738954fed4b8d8ecd8a5f7daeaa2ddb
mileswoolford <mileswoolford@gmail.com>
2016-01-24 12:41:12 -0700

    setup spring container

---
+++ flying-pigs/src/main/java/io/woolford/flyingpigs/Application.java
@@ -0,0 +1,13 @@
+package io.woolford.flyingpigs;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by mwoolford on 1/24/16.
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+// TODO: get spring writing to mysql
